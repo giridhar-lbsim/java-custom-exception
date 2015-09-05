90597bf1d81a7dc9cb92352d7fe84bd40009b148
Giridhar Kumar <giridhar.athena@snapdeal.com>
2015-09-05 06:37:52 +0530

    customoze asddsadf

--- ui/app/js/customize-snapbox/customize-snapbox-ctrl.js
+++ ui/app/js/customize-snapbox/customize-snapbox-ctrl.js
@@ -1,17 +1,27 @@
 angular.module( 'customize-snapbox', ['user'] ).config( function( $stateProvider ) {
-  $stateProvider.state( 'customize-snapbox', {
-    url: '/createsnapbox',
-    templateUrl: 'customize-snapbox/customize-snapbox.tpl',
-    controller: 'CustomizeSnapboxCtrl'
-  } );
+	$stateProvider.state( 'customize-snapbox', {
+		url: '/createsnapbox',
+		templateUrl: 'customize-snapbox/customize-snapbox.tpl',
+		controller: 'CustomizeSnapboxCtrl'
+	} );
 } ).controller( 'CustomizeSnapboxCtrl', function( $scope, $state, CustomizeSnapboxService ) {
-$scope.customizeSnapboxCtrl = {};
-CustomizeSnapboxService.customizeSnapbox().success(function(response){
-console.log("response",response);
-if(response.data){
-	$scope.customizeSnapboxCtrl.productList = response.data;
-}
-});
+	$scope.customizeSnapboxCtrl = {};
+	CustomizeSnapboxService.customizeSnapbox().success(function(response){
+		console.log("response",response);
+		if(response.data){
+			$scope.customizeSnapboxCtrl.productList = response.data;
+		}
+	});
   // console.log("HomeCtrl");
   //   $state.go( 'login' );
+  $scope.check = function(){
+  	var count = $("#hourlyOffrWrper :checked").size();
+  	if(count===0) {
+  		alert("Please select atleast one product");
+  	} else if(count>6) {
+  		alert("You can select maximum 6 products. Please update the list.");
+  	}else {
+		location.href = '/subscription';
+  	}
+  };
 } );
--- ui/app/js/customize-snapbox/customize-snapbox.tpl
+++ ui/app/js/customize-snapbox/customize-snapbox.tpl
@@ -1,1 +1,22 @@
-<div ng-repeat="product in customizeSnapboxCtrl.productList">{{product.title}}</div>
+<h3>Please select any 6 products to create your own snapbox!<h3>
+
+	<div id="hourlyOffrWrper" style="width:1112px;">                    
+		<ul class="floatArea offerGrid">
+			<li ng-repeat="product in customizeSnapboxCtrl.productList" class="item Mobiles box"><div class="OffersContentBox flt">
+				<div class="OffersContentIMG flt"><img height="225" src="http://localhost:8080/{{product.img}}" style="opacity: 1;"></div>
+				<div class="OffersContTxt flt">
+					<div class="OffersTitle flt">{{product.title}}</div>
+					<div class="posAbs disOfrTxt"> 12%<br>Off </div>
+					<div class="OffersContLFT flt">
+						<div class="SingleOfferTxt flt">
+							<span>MRP : <span style="text-decoration:line-through;"> Rs.{{product.mrp}} </span></span><br>
+							Offer price: Rs.{{product.offer}}
+						</div>
+					</div>
+					<input type="checkbox" name="product" value="{{product.SUPC}}">
+				</div>
+			</li>
+		</ul>
+	</div>
+
+	<button  ng-click="check()">Create SnapBox</button>
